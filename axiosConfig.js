aabc7cec144e9a0a8a0605352f3848aca3d8a086
mohit <mohitb12363@gamil.com>
2024-09-09 13:42:03 +0530

    complete

--- axiosConfig.js
+++ axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create an axios instance
 const axiosConfig = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 // Add a request interceptor to include the token in every request
